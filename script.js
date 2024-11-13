48c8724f344a364dd7df6a5216e297c95264984c
WRamin <ramin.w7@outlook.com>
2024-11-12 23:05:13 -0500

    final version

--- script.js
+++ script.js
@@ -3,19 +3,27 @@ let computerScore = 0;
 let currentRoundNumber = 1;
 
 function generateTarget() {
-    let target = Math.floor(Math.random() * 10)
+    return Math.floor(Math.random() * 10)
 }
 
-function compareGuesses(userGuess, compGuess, target) {
-    if (Math.abs(target - userGuess) < Math.abs(target - compGuess)) {
+function compareGuesses(humanGuess, computerGuess, target) {
+    if (Math.abs(target - humanGuess) < Math.abs(target - computerGuess)) {
         return true
-    } else if (Math.abs(target - compGuess) < Math.abs(target - userGuess)) {
-        return true
-    } else if (userGuess === target && compGuess === target) {
+    } else if (Math.abs(target - computerGuess) < Math.abs(target - humanGuess)) {
+        return false
+    } else if (humanGuess <= target && computerGuess <= target) {
         return true
     }
 }
 
-function updateScore(str) {
-    
+function updateScore(winner) {
+    if (winner === 'human') {
+        humanScore += 1
+    } else if (winner === 'computer') {
+        computerScore += 1
+    }
+}
+
+function advanceRound() {
+    currentRoundNumber += 1
 }
