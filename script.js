52f83d550e2520292bc4be9588861a34a879c412
WRamin <ramin.w7@outlook.com>
2024-11-12 22:40:58 -0500

    second commit

--- script.js
+++ script.js
@@ -6,12 +6,16 @@ function generateTarget() {
     let target = Math.floor(Math.random() * 10)
 }
 
-function compareGuesses(userGuess, compGuess, tarGuess) {
-    let closest;
-    if (userGuess === target || compGuess === target) {
-        return  
+function compareGuesses(userGuess, compGuess, target) {
+    if (Math.abs(target - userGuess) < Math.abs(target - compGuess)) {
+        return true
+    } else if (Math.abs(target - compGuess) < Math.abs(target - userGuess)) {
+        return true
+    } else if (userGuess === target && compGuess === target) {
+        return true
     }
-    else if (Math.abs(target - userGuess) < Math.abs(target - compGuess)) {
-        closest = userGuess;
-    } else if (Math.abs())
+}
+
+function updateScore(str) {
+    
 }
